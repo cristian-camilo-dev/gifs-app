23dc2e74bef5b5c61cf75d9d377107e8ddbaad8c
cristian-camilo-dev <cristianrodrig149@gmail.com>
2024-09-16 22:53:23 +0200

    initial proyect

---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-header',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+})
+export class HeaderComponent {}
